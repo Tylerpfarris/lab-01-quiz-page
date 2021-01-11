1d595dd6e424e31b8a3bfde6a8de0b08aeeceae0
Tyler Farris <Tylerpfarris@gmail.com>
2021-01-11 13:27:36 -0800

    working quiz button

---
+++ app.js
@@ -0,0 +1,17 @@
+// import functions and grab DOM elements
+const quizButton = document.getElementById('launch-quiz');
+
+const injectArea = document.getElementById('inject-area');
+
+quizButton.addEventListener('click', () => {
+
+    alert('Welcome!');
+
+    const continuePrompt = prompt('Do you wanna take a quiz?');
+
+});
+
+
+// initialize state
+
+// set event listeners to update state and DOM
